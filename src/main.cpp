3f42b1f95746082a08583d358c182e529e4cb130
reaganak40 <reaganak@gmail.com>
2024-12-07 20:02:27 -0500

    made necessary changes.

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@ void DoDay7();
 
 int main(int argc, char *argv[])
 {
-    //DoDay1();
-    //DoDay2();
-    //DoDay3();
-    //DoDay4();
-    //DoDay5();
-    //DoDay6();
+    DoDay1();
+    DoDay2();
+    DoDay3();
+    DoDay4();
+    DoDay5();
+    DoDay6();
     DoDay7();
     return 0;
 }
